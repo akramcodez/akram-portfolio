3638f81b16c7c9479ba2b24b9a7c25809e9df7a0
akramcodez <skcodewizard786@gmail.com>
2025-07-28 13:07:32 +0530

    project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ThemeSelector } from "@/components/theme-selector";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <ThemeSelector />
+    </div>
+  );
+}
